c2bcf0e54ac3989da10df28b8258bc3bf06bd24f
Bismark Amuna <bismarkamuna007@gmail.com>
2022-09-09 02:32:16 +0000

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,11 @@
 
 /**
  * main -Entry point
- *
+ *Description: Print a string using puts
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 } 	
